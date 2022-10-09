ef33724ac430cadc3c1a375adb5701259929ad2d
muskit <15199219+muskit@users.noreply.github.com>
2022-10-09 15:38:09 -0700

    final

--- Docs/algorithm_python.py
+++ Docs/algorithm_python.py
@@ -5,13 +5,13 @@ def lawnmower(n, l):
   for it in range(iterations):
     # left to right
     for i in range(2*n-1):
-      if l[i] == 1:
+      if l[i] == 1 and l[i+1] == 0:
         temp = l[i]
         l[i] = l[i+1]
         l[i+1] = temp
     # right to left
     for i in range(2*n-1, 0, -1):
-      if l[i] == 0:
+      if l[i] == 0 and l[i-1] == 1:
         temp = l[i]
         l[i] = l[i-1]
         l[i-1] = temp
@@ -20,7 +20,7 @@ def alternate(n, l):
   print(l)
   for i in range(n+1):
     for j in range(i, 2*n-1):
-      if l[j] == 1:
+      if l[j] == 1 and l[j+1] == 0:
         temp = l[j]
         l[j] = l[j+1]
         l[j+1] = temp
